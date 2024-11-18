5a4429e02cc0dcbc9a93e89425e2bdafd1ba3cb2
Nate <natan.vrl@protonmail.com>
2024-11-18 19:26:58 +0100

    working on hero image with svg integration on the background

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,41 @@
 import Image from "next/image";
 import React from "react";
 
+function RingImg(): JSX.Element {
+  return (
+    <div
+      className="w-full h-full"
+      style={{
+        backgroundImage: "url('/images/ring.png')",
+        backgroundPosition: "bottom",
+        backgroundSize: "auto",
+        backgroundRepeat: "repeat-x",
+      }}
+    />
+  );
+}
+
+function MainCharImg(): JSX.Element {
+  return (
+    <div className="absolute w-[600px] h-[500px] bottom-0 right-[10%]">
+    <Image
+      src="/images/hero-main-char.svg"
+      alt="hero image"
+      fill
+      priority
+      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+      className="object-center"
+    />
+    </div>
+
+  );
+}
+
 function Hero(): JSX.Element {
   return (
-    <section className='relative h-[calc(100vh-80px)] w-full'>
-      <div className=''>
-        <Image
-          src='/images/hero-image-test.webp'
-          alt='hero image'
-          fill
-          priority
-          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-          className='object-cover xl:object-fill object-left'
-        />
-      </div>
+    <section className="relative w-full h-[calc(100vh-80px)]">
+      <RingImg />
+      <MainCharImg />
     </section>
   );
 }
